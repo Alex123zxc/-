8907f78caa45e3e4154637463b8a9d71356e9428
zwb <1565277356@qq.com>
2023-01-05 16:43:32 +0800

    23.1.05

--- src/views/dashboard/card/cardItem/index.vue
+++ src/views/dashboard/card/cardItem/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="tem-card">
-    <div class="cardsSvg">
-      <div class="cardSvg" icon="el-icon-s-order">
-      </div>
+    <div class="cardsSvg" :class="bgcCar">
+      <div :class="iconSum"></div>
     </div>
-    <div class="cards"></div>
-    <count-to :start-val="0" :end-val="5000" :duration="2100"></count-to>
+    <div class="card-panel-description">
+      <div class="cards">{{ztext}}</div>
+      <count-to
+        :start-val="startVal"
+        :end-val="toSum"
+        :duration="continues"
+        class="num"
+      ></count-to>
+    </div>
   </div>
 </template>
 
@@ -13,14 +19,15 @@
 import CountTo from "vue-count-to";
 export default {
   name: "CardItem",
+  props: ["iconSum","bgcCar","ztext","toSum","startVal","continues"],
   components: {
     CountTo,
   },
-  props: [],
 };
 </script>
 
 <style scoped>
+/* 整个卡片样式 */
 .tem-card {
   height: 108px;
   cursor: pointer;
@@ -31,5 +38,96 @@ export default {
   background: #fff;
   box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.05);
   border-color: rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
+}
+.tem-card:hover {
+  box-shadow: 6px 6px 10px rgba(179, 177, 177, 0.45);
+}
+/* 包裹图标盒子的样式 */
+.tem-card .cardsSvg {
+  float: left;
+  margin: 14px 0 0 14px;
+  padding: 16px;
+  transition: all 0.38s ease-out;
+  border-radius: 6px;
+}
+/* 鼠标放上后卡片背景颜色 */
+.bgc1:hover {
+  background-color: #038d11;
+}
+.bgc2:hover {
+  background-color: #b2022e;
+}
+.bgc3:hover {
+  background-color: #002FA7;
+}
+.bgc4:hover {
+  background-color: #f1893f;
+}
+/* 具体字体图标 */
+.tem-card .cardsSvg .el-icon-s-custom,
+.el-icon-money,
+.el-icon-s-promotion,
+.el-icon-table-lamp {
+  margin-top:-8px ;
+  float: left;
+  font-size: 60px;
+}
+
+.el-icon-s-custom {
+  background: linear-gradient(
+    to bottom,
+    #038d11,
+    #ffff
+  );
+  -webkit-background-clip: text;
+  color: transparent;
+}
+.el-icon-money {
+  background: linear-gradient(
+    to bottom,
+    #b2022e,
+    #ffff
+  );
+  -webkit-background-clip: text;
+  color: transparent;
+}
+.el-icon-s-promotion{
+  background: linear-gradient(
+    to bottom,
+    #002FA7,
+    #ffff
+  );
+  -webkit-background-clip: text;
+  color: transparent;
+}
+.el-icon-table-lamp{
+  background: linear-gradient(
+    to bottom,
+    #f1893f,
+    #ffff
+  );
+  -webkit-background-clip: text;
+  color: transparent;
+}
+
+
+/* 包裹两处文字的盒子 */
+.card-panel-description {
+  float: right;
+  font-weight: bold;
+  margin: 26px;
+  margin-left: 0px;
+}
+/* 标题 */
+.cards {
+  line-height: 18px;
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 16px;
+  margin-bottom: 12px;
+}
+/* 缓增数字 */
+.num {
+  font-size: 20px;
 }
 </style>
--- src/views/dashboard/card/index.vue
+++ src/views/dashboard/card/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <CardItem />
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <CardItem iconSum="el-icon-s-custom" bgcCar="bgc1" ztext="自习人数" :toSum="126" :startVal="99" :continues="1600"/>
+      </el-col>
+      <el-col :span="6">
+        <CardItem iconSum="el-icon-money" bgcCar="bgc2" ztext="今日收益" :toSum="2465" :startVal="1699" :continues="2000"/>
+      </el-col>
+      <el-col :span="6">
+        <CardItem iconSum="el-icon-s-promotion" bgcCar="bgc3" ztext="当日访问" :toSum="4651" :startVal="2451" :continues="2100"/>
+      </el-col>
+      <el-col :span="6">
+        <CardItem iconSum="el-icon-table-lamp" bgcCar="bgc4" ztext="剩余座位" :toSum="67" :startVal="106" :continues="1300"/>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
