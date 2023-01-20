aa92f00e68f64933f46c8a710a6abb8f0854c8b4
zwb <1565277356@qq.com>
2023-01-20 21:37:56 +0800

    23.1.20

---
+++ src/store/modules/home.js
@@ -0,0 +1,29 @@
+import mockRequest from '@/utils/mockRequest'
+const state = {
+    list:{}
+};
+
+const mutations = {
+    GETSTATUS(state,list){
+        state.list=list;
+    }
+};
+
+const actions = {
+    async getStatus({commit}){
+        let result=await mockRequest.get('/home/list')
+        if(result.code==20000){
+            commit("GETSTATUS",result.data)
+        }
+    }
+};
+
+const getters = {};
+
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters,
+}
--- src/views/dashboard/text/status/index.vue
+++ src/views/dashboard/text/status/index.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>123</div>
+  <el-table style="border-radius:5px">
+    <el-table-column label="订单" min-width="200">
+      <template slot-scope="">
+        {{ 123 }}
+      </template>
+    </el-table-column>
+    <el-table-column label="价格" width="195" align="center">
+      <template slot-scope="">
+        {{ 456 }}
+      </template>
+    </el-table-column>
+    <el-table-column label="状态" width="100" align="center">
+      <template slot-scope="">
+        {{ 789 }}
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
 export default {
-  name: 'Status',
+  name: "Status",
+  mounted(){
+    this.$store.dispatch("getStatus",{})
+  }
 };
 </script>
 
 <style scoped>
-
 </style>
