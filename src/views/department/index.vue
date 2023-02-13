60179a2199b2e0cae2e2fdc2725d1d8d48e9048f
zwb <1565277356@qq.com>
2023-02-13 22:46:33 +0800

    23.2.13

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -110,15 +110,85 @@
         ></el-table-column>
         <el-table-column label="操作" align="center">
           <template slot-scope="scope">
-            <el-button type="text" size="small" @click="getModify()">修改</el-button>
-            <el-button type="text" size="small" @click="getDelete(scope.$index)">删除</el-button>
+            <el-button type="text" size="small" @click="dialogVisible = true"
+              >修改</el-button
+            >
+            <el-button type="text" size="small" @click="getDelete(scope.$index)"
+              >删除</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
       <div class="block">
-        <el-pagination layout="prev, pager, next" :total="50" style="text-align: center;margin: 20px 20px;"> </el-pagination>
+        <el-pagination
+          layout="prev, pager, next"
+          :total="50"
+          style="text-align: center; margin: 20px 20px"
+        >
+        </el-pagination>
       </div>
     </div>
+    <!-- 遮罩层 -->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <el-form ref="form" :model="form" label-width="200px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-col :span="11">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="form.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="form.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="即时配送">
+          <el-switch v-model="form.delivery"></el-switch>
+        </el-form-item>
+        <el-form-item label="活动性质">
+          <el-checkbox-group v-model="form.type">
+            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+            <el-checkbox label="地推活动" name="type"></el-checkbox>
+            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="特殊资源">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="线上品牌商赞助"></el-radio>
+            <el-radio label="线下场地免费"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -304,18 +374,37 @@ export default {
         },
       ],
       multipleSelection: [],
+      // 遮罩层
+      dialogVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
     };
   },
-  methods:{
+  methods: {
     // 删除
-    getDelete(index){
-      this.tableData.splice(index,1)
+    getDelete(index) {
+      this.tableData.splice(index, 1);
     },
-    // 修改
-    getModify(){
-
-    }
-  }
+    // 点击弹出表单
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then((_) => {
+          done();
+        })
+        .catch((_) => {});
+    },
+    onSubmit() {
+      console.log("submit!");
+    },
+  },
 };
 </script>
 
